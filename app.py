6c45f583de63d5b4a8e9ec28e82d2c11407b5245
SERJika <lavrovdoctor@mail.ru>
2020-01-12 17:35:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, abort
 import locale
 
-locale.setlocale(locale.LC_ALL, 'ru_RU.utf8')
+locale.setlocale(locale.LC_ALL, 'ru_RU')
 
 app = Flask(__name__)
 
